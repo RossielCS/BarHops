d8adbb5697c48df04229ec62bd9cdfd3c62aacb2
Rossiel Carranza <rossielcarranza@gmail.com>
2020-06-29 14:22:06 -0300

    Add controllers, views and routes
    - Controllers, views and routes for users, groups, attendances, sessions

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,13 @@
+class AttendancesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,13 @@
+class GroupsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
