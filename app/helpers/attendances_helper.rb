f7e7abaa8e821a6af27e2309ba917ee53cbcf729
Rossiel Carranza <rossielcarranza@gmail.com>
2020-07-02 00:35:32 -0300

    Add style to log in view
    - Add fonts
    - Shows group avatar

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -6,4 +6,9 @@ module AttendancesHelper
   def attn_without_group
     Attendance.includes(:group).where(group: nil, author: current_user.id).order(created_at: :desc)
   end
+
+  def attn_group(group_id)
+    image = Group.where(id: group_id).first.group_avatar
+    image_tag image.variant(resize: '100x100')
+  end
 end
