4f9743c67aafbadac9f602808d3cdcbdb0fb02c0
Rossiel Carranza <rossielcarranza@gmail.com>
2020-07-07 22:02:25 -0300

    Run migration for icon

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_233101) do
+ActiveRecord::Schema.define(version: 2020_07_07_205820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_06_30_233101) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "icon"
     t.index ["name"], name: "index_groups_on_name", unique: true
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
